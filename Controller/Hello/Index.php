64d4e6cfc1717984f8c6579c57e4395ec9262da8
junaidhcs <junaidhcs@yahoo.com>
2023-06-01 12:24:26 +0530

    Update Index.php

--- Controller/Hello/Index.php
+++ Controller/Hello/Index.php
@@ -1,11 +1,30 @@
 <?php 
-namespace Mytask\HelloworldControFront\Block; 
-
-use Magento\Framework\View\Element\Template;                             
- class Hello extends Template
-    { 
-        public function getText()
-        { 
-            return "Hello World"; 
-         }
+    namespace Mytask\HelloworldControFront\Controller\Hello; 
+    use Magento\Framework\App\Action\Action; 
+    use Magento\Framework\App\Action\Context; 
+    use Magento\Framework\View\Result\PageFactory; 
+    class Index extends Action { 
+        /** * @var PageFactory */ 
+         private $pageFactory; 
+         /** 
+                       * Index constructor. 
+                       * @param Context $context  
+                       * @param PageFactory $pageFactory 
+        */               
+        public function __construct
+        ( 
+                Context $context, 
+                PageFactory $pageFactory ) 
+                                             
+                { 
+                    parent::__construct($context);
+                    $this->pageFactory = $pageFactory; 
+                } 
+                /** 
+                 * Execute action based on request and return result * * Note: Request will be added as operation argument in future * * @return \Magento\Framework\Controller\ResultInterface|ResponseInterface * @throws \Magento\Framework\Exception\NotFoundException */ 
+                public function execute()
+                { 
+                    $page = $this->pageFactory->create(); 
+                    return $page;
+                }
     }
